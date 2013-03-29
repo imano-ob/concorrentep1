6da2fc1140d6a4086c263914c1692d29fbbea43c
Renan T. Carneiro <renanteruoc@gmail.com>
2013-03-29 13:10:39 -0300

    More stuffs. Usuarios reais ed git me matarim por essas mensagens de commit.

--- globals.h
+++ globals.h
@@ -1,30 +1,24 @@
 #ifndef __GLOBALS__
 #define __GLOBALS__
 
-pthread_t *atleta;
-pthread_t classificacao;
-int *PortalT1Ent, *PortalT1Sai, *PortalT2Ent, *PortalT2Sai, *estrada[180];
+#define CAT_HOMEM_PRO 0;
+#define CAT_MULHER_PRO 1;
+#define CAT_HOMEM_AMADOR 2;
+#define CAT_MULHER_AMADORA 3;
 
-const int cat_homem_pro = 0;
-const int cat_mulher_pro = 1;
-const int cat_homem_amador = 2;
-const int cat_mulher_amadora = 3;
+#define NUM_CATEGORIAS 4
 
-const int etapa_natacao = 0;
-const int etapa_T1 = 1;
-const int etapa_ciclismo = 2;
-const int etapa_T2 = 3;
-const int etapa_corrida = 4;
+#define ETAPA_NATACAO 0;
+#define ETAPA_T1 1;
+#define ETAPA_CICLISMO 2;
+#define ETAPA_T2 3;
+#define ETAPA_CORRIDA 4;
 
-const int num_etapas = 5;
+#define NUM_ETAPAS 5;
 
-const int ciclismo_subida = 0;
-const int ciclismo_plano = 1;
-const int ciclismo_descida = 2;
-
-int **posicao_atleta;
-/*TODO: Ver o que eu ia fazer com a linha abaixo*/
-/*int **/
+#define CICLISMO_SUBIDA 0;
+#define CICLISMO_PLANO 1;
+#define CICLISMO_DESCIDA 2;
 
 typedef struct{
   int min[4], max[4];
@@ -35,12 +29,17 @@ typedef struct{
     int id;
 }tmpname2;
 
+pthread_t *atleta;
+pthread_t classificacao;
+int *PortalT1Ent, *PortalT1Sai, *PortalT2Ent, *PortalT2Sai, *estrada[180];
+
+int **posicao_atleta;
 
 velocidades *velocidades_etapa[5];
 
 int num_atletas;
 int homens_pro, mulheres_pro, homens_amadores, mulheres_amadoras;
 
-/*Funcoes para as threads*/
+int initialized;
 
 #endif /*__GLOBALS__*/
--- main.c
+++ main.c
@@ -1,86 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
-pthread_t *atleta;
-pthread_t classificacao;
-int *PortalT1Ent, *PortalT1Sai, *PortalT2Ent, *PortalT2Sai, *estrada[180];
-
-const int cat_homem_pro = 0;
-const int cat_mulher_pro = 1;
-const int cat_homem_amador = 2;
-const int cat_mulher_amadora = 3;
-
-const int etapa_natacao = 0;
-const int etapa_T1 = 1;
-const int etapa_ciclismo = 2;
-const int etapa_T2 = 3;
-const int etapa_corrida = 4;
-
-const int ciclismo_subida = 0;
-const int ciclismo_plano = 1;
-const int ciclismo_descida = 2;
-
-int **posicao_atleta;
-
-typedef struct{
-  int min[4], max[4];
-}velocidades;
-
-velocidades *velocidades_etapa[5];
-
-int num_atletas;
-int homens_pro, mulheres_pro, homens_amadores, mulheres_amadoras;
-
-/*Funcoes para as threads*/
-
-void correr(void *arg){
-  int categoria;
-  categoria = (int *)*arg;
-  int etapa_atual = 0;
-  int distancia_percorrida = 0;
-  int tempo_atual = 0;
-  int done = 0;
-  int velocidade_atual;
-  velocidades velocidades_ref;
-  while(!done){
-    while( distancia_percorrida < distancia_etapa[etapa_atual]){
-      if (etapa_atual == etapa_ciclismo){
-	terreno_atual = 1; /*TODO: Verificar se subida, descida ou plano*/
-	velocidades_ref = velocidades_etapa[etapa_ciclismo][terreno_atual];
-      }
-      else
-	velocidades_ref = velocidades_etapa[etapa_atual][0];
-      /*velocidade atual = random entre min e max*/
-      distancia_percorrida += /*something*/ 100;
-      tempo_atual += velocidade_atual /*mudar esse nome?*/
-      if (/*Time elapsed since last announce > meia hora(ou minuto em debug)*/ 1){
-        /*Calcula onde tava na meia hora*/
-        /*Await poder anunciar*/
-        /*Anuncia*/
-      }
-    }
-    etapa_atual++;
-    if( etapa_atual <= num_etapas )
-    /*Sync*/
-    /*Await go*/
-  }
-}
-
-void anunciar(void *args){
-  int i;
-  while (/*prova nao termina*/ 1){
-    for (i = 0; i < num_atletas; i++){
-      /*Await ter um numero anunciado*/
-      /*Guarda esse numero no vetor da categoria*/
-      /*Zera aquele numero*/
-    }
-    for(i = 0; i < num_categorias; i++){
-      /*sort no vetor*/
-      /*anuncia top 3 ou top everyone no debug*/
-    }
-  }
-}
+#include "globals.h"
 
 velocidades inicaliza_vels(int hp_min, int hp_max, int mp_min, int mp_max, int ha_min, int ha_max,
                            int ma_min, int ma_max){
@@ -97,40 +18,29 @@ velocidades inicaliza_vels(int hp_min, int hp_max, int mp_min, int mp_max, int h
 }
 
 int main(int argc, char **argv){
-  int i, atletas_inicializados = 0;
+  int i, j, id_atleta = 0;
+  int participantes_categoria[NUM_CATEGORIAS]
+  tmpname2 tmpname;
   /*Readfiles*/
   /*Inicializar coisas*/
+  srand(time(NULL));
   if(pthread_create(classificacao), NULL, &anunciar, NULL){
     fprintf(stderr, "Erro ao criar thread da classificacao\n");
     exit(EXIT_FAILURE);
   }
-  for (i = 0; i < homens_pro; i++){
-    if(pthread_create(atleta[atletas_inicalizados], NULL, correr, &cat_homem_pro)){
-      fprintf(stderr, "Erro ao criar thread do atleta\n");
-      exit(EXIT_FAILURE);
+  for (i = 0; i < NUM_CATEGORIAS; i++){
+    for (j = 0; j < participants_categoria[i]; j++){
+      initialized = 0;
+      tmpname.categoria = i;
+      tmpname.id = id_atleta;
+      if(pthread_create(atleta[id_atleta], NULL, &correr, &tmpname)){
+	fprintf(stderr, "Erro ao criar thread do atleta\n");
+	exit(EXIT_FAILURE);
+      }
+      while (!initialized) 
+	sleep(1);
+      id_atleta++;
     }
-    atletas_inicializados++;
-  }
-  for (i = 0; i < mulheres_pro; i++){
-    if(pthread_create(atleta[atletas_inicalizados], NULL, correr, &cat_mulher_pro)){
-      fprintf(stderr, "Erro ao criar thread do atleta\n");
-      exit(EXIT_FAILURE);
-    }
-    atletas_inicializados++;
-  }
-    for (i = 0; i < homens_amadores; i++){
-      if(pthread_create(atleta[atletas_inicalizados], NULL, correr, &cat_homem_amador)){
-      fprintf(stderr, "Erro ao criar thread do atleta\n");
-      exit(EXIT_FAILURE);
-    }
-    atletas_inicializados++;
-  }
-  for (i = 0; i < mulheres_amadoras; i++){
-    if(pthread_create(atleta[atletas_inicalizados], NULL, correr, &cat_mulher_amadora)){
-      fprintf(stderr, "Erro ao criar thread do atleta\n");
-      exit(EXIT_FAILURE);
-    }
-    atletas_inicializados++;
   }
   /*Wait...*/
 }
